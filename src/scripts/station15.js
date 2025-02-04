58b43f6cb92dabc9876aa937f25eed51b6acb887
Octleas <s.naito.0504@gmail.com>
2025-02-04 12:12:17 +0900

    clear

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,44 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(userList.map(buildFullName)), 3000);
+  });
 }
 
+function buildFullName({
+  id,
+  first_name,
+  family_name,
+  affiliation,
+  is_student,
+}) {
+  return {
+    id,
+    full_name: `${family_name} ${first_name}`,
+    first_name,
+    family_name,
+    affiliation,
+    is_student,
+  };
+}
